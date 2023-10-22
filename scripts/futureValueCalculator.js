8a185eeb36cc7875af6d58ecccd16c0bfc21943f
MrRobit <armaniefel98@gmail.com>
2023-10-22 15:18:47 -0400

    created a compound interest calculator that also tells you your interest gained

--- scripts/futureValueCalculator.js
+++ scripts/futureValueCalculator.js
@@ -1,3 +1,23 @@
-const principle = 53000;
-const interestRate = 7.625 /12;
-const loanTerm = 15 * 12;
+window.onload = handleOnload;
+
+function handleOnload(){
+    const calculateButtonEl = document.getElementById("calculateButton");
+
+    calculateButtonEl.onclick = handleCalculateOnclick;
+}
+
+function handleCalculateOnclick(){
+    console.log("calculate button clicked");
+    const depositInputEl = document.getElementById("deposit");
+    const interestRateInputEl = document.getElementById("interestRate");
+    const loanTermInputEl = document.getElementById("loanTerm");
+    const futurevalueInputEl = document.getElementById("futureValue");
+    const interestEarnedinputEl = document.getElementById("interestEarned");
+
+    const deposit = Number(depositInputEl.value);
+    const interestRate = Number(interestRateInputEl.value) / 100; 
+    const loanTerm = Number(loanTermInputEl.value);
+
+     futurevalueInputEl.value = deposit * Math.pow(1 + interestRate / 365, 365 * 5);
+    interestEarnedinputEl.value = deposit * Math.pow(1 + interestRate / 365, 365 * 5)- deposit;
+}
